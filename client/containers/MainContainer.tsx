a6f8dd696ab553276d2216f96e3651105353534e
morgan562 <tylermor562@gmail.com>
2020-10-01 19:33:46 -0700

    updated redux functional components
    Co-authored-by: Joal Kim <joalkims@gmail.com>
    Co-authored-by: Sophia Pak <sophiakpak@gmail.com>
    Co-authored-by: Tommy Song <tommysong123@gmail.com>

---
+++ client/containers/MainContainer.tsx
@@ -0,0 +1,13 @@
+
+
+
+import React from 'react'
+
+const MainContainer:React.FC = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+export default MainContainer
---
+++ client/reducers/reducer.ts
@@ -0,0 +1,12 @@
+import reducers from ".";
+import actions from "../actions/actions";
+
+const initialState = {
+
+};
+
+const reducer = (state = initialState, action = {}) => {
+//set up switch statement 
+}
+export default reducer; 
+
