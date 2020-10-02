dbbc6843c12eaa19e47123fa0a478a663cdf8ebb
sophiapak <sophiakpak@gmail.com>
2020-10-02 16:08:52 -0700

    verified working server router, setup controllers and dotenv
    Co-authored-by: Joal Kim <joalkims@gmail.com>
    Co-authored-by: Tommy Song <tommysong123@gmail.com>
    Co-authored-by: Tyler Morgan <tylermor562@gmail.com>

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import dotenv from 'dotenv';
 
-const app = express();
+const app: express.Application = express();
+
+// INITIALIZE CONFIGURATION
+dotenv.config();
+
+const PORT = process.env.SERVER_PORT;
+
+// SEND IMPORTED/INPUTTED SCHEMAS TO CLOUD DB
+app.post('/results', (req, res) => {
+  // SENDING CLIENT THE RESULTS FROM THE PERFORMANCE TEST
+});
 
 // TESTING TO MAKE SURE ROUTE IS WORKING - VERIFIED!
 app.get('/test', (req, res) => {
@@ -10,6 +21,6 @@ app.get('/test', (req, res) => {
 });
 
 // LISTENING TO SERVER ON PORT 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('stratosDB server is running on port 3000');
 });
