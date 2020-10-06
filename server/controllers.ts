035111c8908e8027a04978aa135e30e0f75245dc
sophiapak <sophiakpak@gmail.com>
2020-10-06 15:27:25 -0700

    updated the handleChange for Submit Button.
    Co-authored-by: Joal Kim <joalkims@gmail.com>
    Co-authored-by: Tommy Song <tommysong123@gmail.com>
    Co-authored-by: Tyler Morgan <tylermor562@gmail.com>

--- server/controllers.ts
+++ server/controllers.ts
@@ -1,6 +1,6 @@
-import express from "express";
+import express from 'express';
 // NEED TO IMPORT DB FROM MODELS (HAVEN'T DUE TO TS COMPILE ERROR)
-const db = require("./models");
+const db = require('./models');
 
 // DECLARING DATA TYPES FOR ALL THE CONTROLLERS IN STRATOSCONTROLLER
 interface controllers {
@@ -18,31 +18,35 @@ export const stratosController: controllers = {
     res: express.Response,
     next: express.NextFunction
   ) => {
-    console.log("in getResults");
+    console.log('in getResults');
     const { schemaEntry } = req.body;
-
+    // console.log('REQ.BODY: ', req.body);
+    // console.log('SCHEMAENTRY: ', schemaEntry);
     const newData: dataType = {
-      queryData: "",
-      queryStatistics: "",
+      queryData: '',
+      queryStatistics: '',
     };
 
     db.query(schemaEntry)
+      .then((result: any) => {
+        console.log('RESULT: ', result);
+      })
       .then((queryData: any) => {
         newData.queryData = queryData.rows;
 
-        console.log("queryData.rows: ", queryData.rows);
+        console.log('queryData.rows: ', queryData.rows);
 
         // Running Explain for tests
-        db.query("EXPLAIN (FORMAT JSON, ANALYZE) " + schemaEntry).then(
+        db.query('EXPLAIN (FORMAT JSON, ANALYZE) ' + schemaEntry).then(
           (queryStatistics: any) => {
             newData.queryStatistics = queryStatistics.rows;
-            console.log("Returned query stats: ", newData.queryStatistics);
+            console.log('Returned query stats: ', newData.queryStatistics);
           }
         );
       })
       .catch((error: string) => {
         console.log(
-          "Error in Controllers getResults Query Entry and Analysis: ",
+          'Error in Controllers getResults Query Entry and Analysis: ',
           error
         );
       });
