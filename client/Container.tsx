98bc2ee876deb093f4ac1f4fc81dbd8f95f47ef9
joalk <joalkims@gmail.com>
2020-10-07 21:46:15 -0700

    added line graph in right-panel
    Co-authored-by: Sophia Pak <sophiakpak@gmail.com>
    Co-authored-by: Tommy Song <tommysong123@gmail.com>
    Co-authored-by: Tyler Morgan <tylermor562@gmail.com>

--- client/Container.tsx
+++ client/Container.tsx
@@ -2,36 +2,55 @@ import React, { Component, SyntheticEvent } from 'react';
 import Announcement from './components/Announcement';
 import CodeEditor from './components/CodeEditor';
 import Sidebar from './components/Sidebar';
+import LineGraph from './components/LineGraph';
 import axios from 'axios';
 
-interface obj {
-  name: string;
+interface ContainerState {
+  queries: {
+    queryString: string;
+    queryData: {}[];
+    queryStatistics: any;
+    querySchema: string;
+    queryLabel: string;
+  }[];
   //Announcement
   announcement: string;
   //codeEditorState
   schemaEntry: string;
-  data: object;
   onClose: any;
   schemaName: string;
   //sideBar
   url: string;
 }
 
-class Container extends Component<{}, obj> {
-  constructor(props: any) {
+class Container extends Component<{}, ContainerState> {
+  constructor(props: {}) {
     super(props);
-    this.state = {
-      name: 'Tyler',
-      announcement: 'Welcome to StratosDB',
-      schemaEntry: '',
-      data: {},
-      onClose: true,
-      schemaName: '',
-      url: '',
-    };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.refresh = this.refresh.bind(this);
+  }
+
+  state: ContainerState = {
+    queries: [],
+    announcement: 'Welcome to StratosDB',
+    schemaEntry: '',
+    onClose: true,
+    schemaName: '',
+    url: '',
+  };
+
+  componentDidMount() {
+    const newQuery = {
+      queryString: 'hey',
+      queryData: [{ test: 'test' }],
+      queryStatistics: 'is',
+      querySchema: 'a',
+      queryLabel: 'test',
+    };
+    let queries = this.state.queries.slice();
+    this.setState({ queries });
   }
 
   handleChange(event: string) {
@@ -47,43 +66,46 @@ class Container extends Component<{}, obj> {
     const schemaObj = {
       schemaEntry: this.state.schemaEntry,
     };
-
-    axios.post('/results', schemaObj).then((data) => {
-      console.log('logging data', data.data);
-      this.setState({ data: data });
-    });
+    console.log('queryData', schemaObj);
+    // axios.post('/results', schemaObj).then((data) => {
+    //   console.log('logging data', data.data);
+    //   this.setState({ queries.queryData: data });
+    // });
   }
   // possibly needs componenet did update
   refresh(event: React.ChangeEvent<HTMLSelectElement>) {
     event.preventDefault();
     window.location.reload(false);
     // this.setState({
-    //   name: 'Tyler', 
-    //   announcement: 'Welcome to StratosDB',   
-    //   schemaEntry: '', 
+    //   name: 'Tyler',
+    //   announcement: 'Welcome to StratosDB',
+    //   schemaEntry: '',
     //   data:{},
     //   onClose: true,
     //   schemaName: '',
     //   url: ''
     // })
-    console.log("refreshing");
+    console.log('refreshing');
   }
   render() {
     return (
-      <div id="main-container">
-        <div id="left-panel">
+      <div id='main-container'>
+        <div id='left-panel'>
           <Sidebar url={this.state.url} refresh={this.refresh} />
         </div>
-        <div id="right-panel">
+        <div id='right-panel'>
           <Announcement announcement={this.state.announcement} />
-          <CodeEditor
-            schemaEntry={this.state.schemaEntry}
-            data={this.state.data}
-            onClose={this.state.onClose}
-            schemaName={this.state.schemaName}
-            handleChange={this.handleChange}
-            handleSubmit={this.handleSubmit}
-          />
+          <div id='main-feature'>
+            <CodeEditor
+              schemaEntry={this.state.schemaEntry}
+              data={this.state.queries}
+              onClose={this.state.onClose}
+              schemaName={this.state.schemaName}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
+            <LineGraph queries={this.state.queries} />
+          </div>
         </div>
       </div>
     );
