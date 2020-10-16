f4f00bbda3f6fe94e59c1bc5c6beaa17f4ea78b4
morgan562 <tylermor562@gmail.com>
2020-10-15 19:08:24 -0700

    change to line graph logic quote> quote> quote> Co-authored-by: Joal Kim <joalkims@gmail.com> Co-authored-by: Sophia Pak <sophiakpak@gmail.com> Co-authored-by: Tommy Song <tommysong123@gmail.com>

--- client/components/LineGraph.tsx
+++ client/components/LineGraph.tsx
@@ -13,7 +13,6 @@ class LineGraph extends Component<LineGraphProps> {
 
   // componentDidUpdate() {
   //   // const labelData = () => {};
-  //   const execTime: any = 'Execution Time';
   //   const execTimeArr: number[] = [];
   //   const runtimeData = (results: any) => {
   //     const newData: any = this.props.queries[execTime];
@@ -23,10 +22,12 @@ class LineGraph extends Component<LineGraphProps> {
   //     queryStatistics: execTimeArr,
   //   });
   // }
-
+  const execTime: any = 'Execution Time';
+  
   render() {
     const { queries } = this.props;
-    // const labelData = () => queries.map((query) => query.queryLabel);
+  
+    //  const labelData = () => queries.map((query) => query.queryLabel);
     // const runtimeData = () => queries.map((query) => queries);
     const data = {
       labels: ['Query1', 'Query2', 'Query3', 'Query4'],
@@ -38,13 +39,14 @@ class LineGraph extends Component<LineGraphProps> {
           backgroundColor: '#399cff',
           borderColor: 'rgba(247,247,247,247)',
           borderWidth: 2,
-          data: [0.2, 0.5, 1, 0.1],
+          data: [queries.execution],
         },
       ],
     };
 
     return (
       <div id='line-graph' className='LineGraph'>
+        console.log('here lies queries', queries)
         <Line
           data={data}
           options={{
