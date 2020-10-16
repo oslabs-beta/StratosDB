a53e0a038a3115874172e04a8c70c2a20d05c641
sophiapak <sophiakpak@gmail.com>
2020-10-16 12:41:48 -0700

    added controller to run user inputted query before running statistics controller.
    Co-authored-by: Joal Kim <joalkims@gmail.com>
    Co-authored-by: Tommy Song <tommysong123@gmail.com>
    Co-authored-by: Tyler Morgan <tylermor562@gmail.com>

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,19 @@
-import express from "express";
-import bodyParser from "body-parser";
-import path from "path";
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
 import dotenv from 'dotenv';
-import { stratosController } from "./controllers";
+import { stratosController } from './controllers';
 const { Pool } = require('pg');
 
 // INITIALIZE CONFIGURATION
 dotenv.config();
 
 const app: express.Application = express();
+
+interface queryResultObjType {
+  queryStatistics: any;
+  queryTable: any;
+}
 
 // FOR PRODUCTION
 // INTERFACE FOR AWS INFORMATION
@@ -22,11 +27,11 @@ const app: express.Application = express();
 
 // OBJECT CONTAINING AWS INFO FROM THE FRONT END
 let awsInfo: awsTypes = {
-  user: "",
-  host: "",
-  database: "",
-  password: "",
-  port: "",
+  user: '',
+  host: '',
+  database: '',
+  password: '',
+  port: '',
 };
 
 // FOR TESTING
@@ -36,7 +41,7 @@ interface awsTypes {
   database: any;
   password: any;
   port: any;
-};
+}
 
 // let awsInfo: awsTypes = {
 //   user: process.env.RDS_USER,
@@ -64,21 +69,21 @@ app.use(bodyParser.json());
 const PORT = 3000;
 
 // WHEN REFRESHED, THE APP WILL WIPE ANY EXISTING TABLES IN THE DB
-app.get("/refresh", (req, res) => {
+app.get('/refresh', (req, res) => {
   awsInfo = {
-    user: "",
-    host: "",
-    database: "",
-    password: "",
-    port: "",
+    user: '',
+    host: '',
+    database: '',
+    password: '',
+    port: '',
   };
-  console.log("refreshed: ", awsInfo)
-  res.status(200).send("DATABASE HAS A CLEAN SLATE");
+  console.log('refreshed: ', awsInfo);
+  res.status(200).send('DATABASE HAS A CLEAN SLATE');
 });
 
 // CONNECTING TO AWS
-app.post("/connect", (req, res) => {
-  console.log("Incoming form information: ", req.body)
+app.post('/connect', (req, res) => {
+  console.log('Incoming form information: ', req.body);
   // awsInfo = {
   //   user: process.env.RDS_USER,
   //   host: process.env.RDS_ENDPOINT,
@@ -90,34 +95,43 @@ app.post("/connect", (req, res) => {
   // db["query"] = (text: string, params?: any, callback?: any) => {
   //     return pool.query(text, params, callback);
   //   };
-  
+
   // console.log("refreshed: ", awsInfo)
-  res.status(200).send("HOOPLAH MAGIC");
+  res.status(200).send('HOOPLAH MAGIC');
 });
 // PASSING AWS DATABASE INFORMATION INTO SERVER FROM STATE
-app.post("/aws", (req, res) => {
+app.post('/aws', (req, res) => {
   // assigning AWS info to awsInfo
   awsInfo = req.body.awsInfo;
-  console.log("AWS info set: ", awsInfo);
-  res.status(200).send("AWS info has been set")
-})
+  console.log('AWS info set: ', awsInfo);
+  res.status(200).send('AWS info has been set');
+});
 
 // FRONTEND BUTTON THAT WILL ALLOW USER TO DROP ALL TABLES FROM DB
-app.get("/reset", stratosController.reset, (req, res) => {
-  res.status(200).send("DATABASE HAS BEEN RESET");
+app.get('/reset', stratosController.reset, (req, res) => {
+  res.status(200).send('DATABASE HAS BEEN RESET');
 });
 
 // SEND IMPORTED/INPUTTED SCHEMAS TO CLOUD DB
-app.post("/newSchema", stratosController.createSchema, (req, res) => {
+app.post('/newSchema', stratosController.createSchema, (req, res) => {
   // SENDING CLIENT STATUS FOR SCHEMA CREATION
-  res.status(200).send("success");
+  res.status(200).send('success');
 });
 
 // RUNNING TESTS ON THE SCHEMAS IN THE CLOUD
-app.post("/results", stratosController.runTest, (req, res) => {
-  // SENDING CLIENT THE RESULTS FROM THE PERFORMANCE TEST
-  res.status(200).send(res.locals.explain);
-});
+app.post(
+  '/results',
+  stratosController.queryTable,
+  stratosController.runTest,
+  (req, res) => {
+    const queryResultObj: queryResultObjType = {
+      queryStatistics: res.locals.explain,
+      queryTable: res.locals.queryResult,
+    };
+    // SENDING CLIENT THE RESULTS FROM THE PERFORMANCE TEST
+    res.status(200).send(queryResultObj);
+  }
+);
 
 // LISTENING TO SERVER ON PORT 3000
 app.listen(PORT, () => {
