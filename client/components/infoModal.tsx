4e8a8a7efdbd1fd5ab487da4b8277f1095ffe1b7
sophiapak <sophiakpak@gmail.com>
2020-10-17 13:56:00 -0700

    set the initial global styling for infoModal

--- client/components/infoModal.tsx
+++ client/components/infoModal.tsx
@@ -34,40 +34,42 @@ const InfoModal: React.FC<infoModalState> = (props: infoModalState) => {
         style={customStyles}
         contentLabel="Info Modal"
       >
-        <h1>WELCOME TO STRATOSDB</h1>
-        <h2>GETTING STARTED</h2>
-        <h3>STEP ONE</h3>
-        <p>Click on the Cloud Icon (located on the Left Sidebar)</p>
-        <h3>STEP TWO</h3>
-        <p>
-          Enter your AWS RDS Information and press <strong>Connect</strong> to
-          establish a link to your cloud hosted database
-        </p>
-        <p>
-          <strong>User:</strong> Database Username
-          <br />
-          <strong>Host:</strong> AWS RDS Endpoint Link *
-          <br />
-          <strong>Database:</strong> Database name **
-          <br />
-          <strong>Password:</strong> Database Password
-        </p>
-        <p>
-          * Sourced from AWS RDS Database Instance Dashboard
-          <br />
-          ** If Database name was left blank when the AWS RDS Datbase Instance
-          was created, use <strong>postgres</strong>
-        </p>
-        <h2>OVERVIEW</h2>
-        <h3>IMPORTING .SQL & .TAR FILES</h3>
-        <p>Click on the Cloud Icon (located on the Left Sidebar)</p>
-        <h3>SCHEMA CODE EDITOR</h3>
-        <p>Click on the Cloud Icon (located on the Left Sidebar)</p>
-        <h3>QUERY TEXT EDITOR</h3>
-        <p>Click on the Cloud Icon (located on the Left Sidebar)</p>
-        <button className="secondary-button" onClick={props.closeModal}>
-          Close
-        </button>
+        <div id="info-composition">
+          <button className="secondary-button" onClick={props.closeModal}>
+            <strong>x</strong>
+          </button>
+          <h1>WELCOME TO STRATOSDB</h1>
+          <h2>GETTING STARTED</h2>
+          <h3>STEP ONE</h3>
+          <p>Click on the Cloud Icon (located on the Left Sidebar)</p>
+          <h3>STEP TWO</h3>
+          <p>
+            Enter your AWS RDS Information and press <strong>Connect</strong> to
+            establish a link to your cloud hosted database
+          </p>
+          <p>
+            <strong>User:</strong> Database Username
+            <br />
+            <strong>Host:</strong> AWS RDS Endpoint Link *
+            <br />
+            <strong>Database:</strong> Database name **
+            <br />
+            <strong>Password:</strong> Database Password
+          </p>
+          <p>
+            * Sourced from AWS RDS Database Instance Dashboard
+            <br />
+            ** If Database name was left blank when the AWS RDS Datbase Instance
+            was created, use <strong>postgres</strong>
+          </p>
+          <h2>OVERVIEW</h2>
+          <h3>IMPORTING .SQL & .TAR FILES</h3>
+          <p>Click on the Cloud Icon (located on the Left Sidebar)</p>
+          <h3>SCHEMA CODE EDITOR</h3>
+          <p>Click on the Cloud Icon (located on the Left Sidebar)</p>
+          <h3>QUERY TEXT EDITOR</h3>
+          <p>Click on the Cloud Icon (located on the Left Sidebar)</p>
+        </div>
       </Modal>
     </div>
   );
