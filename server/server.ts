42aab478e731f0585eb88ac43b303f36755221b7
tysong24 <tommysong123@gmail.com>
2020-10-17 17:22:17 -0700

    Upload popup to upload from computer pops up successfully.
    Co-authored-by: Joal Kim <joalkims@gmail.com>
    Co-authored-by: Sophia Pak <sophiakpak@gmail.com>
    Co-authored-by: Tyler Morgan <tylermor562@gmail.com>

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { stratosController } from './controllers';
-const { Pool } = require('pg');
+import express from "express";
+import bodyParser from "body-parser";
+import { stratosController } from "./controllers";
+const { Pool } = require("pg");
 const app: express.Application = express();
+const multer = require("multer");
+const cors = require("cors");
 
 /**
  * TYPESCRTIPT INTERFACE DECLARACTIONS
@@ -24,11 +26,11 @@ const PORT = 3000;
 
 // OBJECT CONTAINING AWS INFO FROM THE FRONT END
 let awsInfo: awsTypes = {
-  user: '',
-  host: '',
-  database: '',
-  password: '',
-  port: '',
+  user: "",
+  host: "",
+  database: "",
+  password: "",
+  port: "",
 };
 
 // DECLARING INITIAL POOL VARIABLE THAT WILL BE UPDATED ONCE APPLICATION REFRESHES
@@ -38,27 +40,50 @@ let pool: any;
 const db: any = {};
 
 app.use(bodyParser.json());
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req: any, file: any, cb: any) {
+    cb(null, "public");
+  },
+  filename: function (req: any, file: any, cb: any) {
+    cb(null, Date.now() + "-" + file.originalName);
+  },
+});
+
+const upload = multer({ storage: storage }).single("file");
+
+app.post("/upload", (req: any, res: any) => {
+  upload(req, res, (err: any) => {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+  });
+  return res.status(200).send(req.file);
+});
 
 /**
  * APP.GET REQUEST (/REFRESH): WHEN REFRESHED, THE APP WILL RESET AWS CONNECTION INFO TO EMPTY VALUES
  */
-app.get('/refresh', (req, res) => {
+app.get("/refresh", (req, res) => {
   awsInfo = {
-    user: '',
-    host: '',
-    database: '',
-    password: '',
-    port: '',
+    user: "",
+    host: "",
+    database: "",
+    password: "",
+    port: "",
   };
-  console.log('refreshed: ', awsInfo);
-  res.status(200).send('DATABASE CONNECTION HAS BEEN RESET');
+  console.log("refreshed: ", awsInfo);
+  res.status(200).send("DATABASE CONNECTION HAS BEEN RESET");
 });
 
 /**
  * APP.POST REQUEST (/CONNECT): USER WILL CONNECT TO DB BASED ON THEIR INPUTTED AWS RDS INFORMATION
  */
-app.post('/connect', (req, res) => {
-  console.log('Incoming form information: ', req.body);
+app.post("/connect", (req, res) => {
+  console.log("Incoming form information: ", req.body);
   // REASSIGNING AWSINFO PROPERTY VALUES TO THE USER INPUTTED VALUES FROM THE FRONT END SIDEBAR CLOUD BUTTON MODAL
   awsInfo = {
     user: req.body.user,
@@ -70,26 +95,26 @@ app.post('/connect', (req, res) => {
   // ESTABLISHING A POOL CONNECTION BASED ON OUR NEWLY INPUTTED DB CONNECTION INFORMATION
   pool = new Pool(awsInfo);
   // ADDING A NEW PROPERTY TO OUR DB VARIABLE WITH OUR POOL METHOD THAT WILL ALLOW US TO QUERY OUR DB
-  db['query'] = (text: string, params?: any, callback?: any) => {
+  db["query"] = (text: string, params?: any, callback?: any) => {
     return pool.query(text, params, callback);
   };
-  console.log('HOOPLAH MAGIC: ', awsInfo, 'We have connected!');
+  console.log("HOOPLAH MAGIC: ", awsInfo, "We have connected!");
   res.status(200);
 });
 
 /**
  * APP.POST REQUEST (/NEWSCHEMA): SEND IMPORTED/INPUTTED SCHEMAS TO CLOUD DB
  */
-app.post('/newSchema', stratosController.createSchema, (req, res) => {
+app.post("/newSchema", stratosController.createSchema, (req, res) => {
   // SENDING CLIENT STATUS FOR SCHEMA CREATION
-  res.status(200).send('success');
+  res.status(200).send("success");
 });
 
 /**
  * APP.POST REQUEST (/RESULTS): RUNNING QUERY REQUEST & TESTS ON THE SCHEMAS IN THE CLOUD
  */
 app.post(
-  '/results',
+  "/results",
   stratosController.queryTable,
   stratosController.runTest,
   (req, res) => {
