34b78b18790b9152e96e52fdb0824b5f45c562c9
joalk <joalkims@gmail.com>
2020-10-20 15:26:14 -0700

    refactored Announcement component
    Co-authored-by: Sophia Pak <sophiakpak@gmail.com>
    Co-authored-by: Tommy Song <tommysong123@gmail.com>
    Co-authored-by: Tyler Morgan <tylermor562@gmail.com>

--- __tests__/enzyme.tsx
+++ __tests__/enzyme.tsx
@@ -3,8 +3,9 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Container from '../client/Container';
 import LineGraph from '../client/components/LineGraph';
+import Sidebar from '../client/components/Sidebar';
 import { propTypes } from 'react-codemirror';
-// // import toJson from 'enzyme-to-json';
+// import toJson from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
 
@@ -13,7 +14,16 @@ describe('renders without crashing', () => {
     queryHistory: ['SELECT * FROM address;'];
   });
 
-  it('renders Container', () => {
+  xit('renders Container', () => {
+    shallow(<Container />);
+  });
+  it('renders LineGraph', () => {
     shallow(<LineGraph />);
   });
 });
+
+// describe('math', () => {
+//   it('test', () => {
+//     expect(2).toEqual(2);
+//   });
+// });
--- client/components/CodeEditor.tsx
+++ client/components/CodeEditor.tsx
@@ -31,7 +31,7 @@ const CodeEditor: React.FC<CodeEditorState> = (props: CodeEditorState) => {
   const injectCheck = () => {
     if (props.injectedCode) {
       return (
-        <div className="code-mirror-container">
+        <div className='code-mirror-container'>
           <CodeMirror
             value={props.injectedCode}
             onChange={(e) => props.schemaChange(e)}
@@ -41,7 +41,7 @@ const CodeEditor: React.FC<CodeEditorState> = (props: CodeEditorState) => {
       );
     } else {
       return (
-        <div className="code-mirror-container">
+        <div className='code-mirror-container'>
           <CodeMirror
             onChange={(e) => props.schemaChange(e)}
             options={options}
@@ -52,28 +52,28 @@ const CodeEditor: React.FC<CodeEditorState> = (props: CodeEditorState) => {
   };
 
   return (
-    <div id="code-editor">
+    <div id='code-editor'>
       {injectCheck()}
-      <div id="buttons-section">
+      <div id='buttons-section'>
         <svg
-          id="cancel-button"
+          id='cancel-button'
           onClick={props.emptyInject}
-          xmlns="http://www.w3.org/2000/svg"
-          width="75"
-          height="75"
-          viewBox="0 0 24 24"
+          xmlns='http://www.w3.org/2000/svg'
+          width='75'
+          height='75'
+          viewBox='0 0 24 24'
         >
-          <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.094l-4.157-4.104 4.1-4.141-1.849-1.849-4.105 4.159-4.156-4.102-1.833 1.834 4.161 4.12-4.104 4.157 1.834 1.832 4.118-4.159 4.143 4.102 1.848-1.849z" />
+          <path d='M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.094l-4.157-4.104 4.1-4.141-1.849-1.849-4.105 4.159-4.156-4.102-1.833 1.834 4.161 4.12-4.104 4.157 1.834 1.832 4.118-4.159 4.143 4.102 1.848-1.849z' />
         </svg>
         <svg
-          id="submit-button"
+          id='submit-button'
           onClick={props.schemaSubmit}
-          xmlns="http://www.w3.org/2000/svg"
-          width="75"
-          height="75"
-          viewBox="0 0 24 24"
+          xmlns='http://www.w3.org/2000/svg'
+          width='75'
+          height='75'
+          viewBox='0 0 24 24'
         >
-          <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 17.292l-4.5-4.364 1.857-1.858 2.643 2.506 5.643-5.784 1.857 1.857-7.5 7.643z" />
+          <path d='M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 17.292l-4.5-4.364 1.857-1.858 2.643 2.506 5.643-5.784 1.857 1.857-7.5 7.643z' />
         </svg>
       </div>
     </div>
