335977702ea545a203d633be5b782a17815a42e8
morgan562 <tylermor562@gmail.com>
2020-10-20 11:29:32 -0700

    one working test
    Co-authored-by: Joal Kim <joalkims@gmail.com>
    Co-authored-by: Sophia Pak <sophiakpak@gmail.com>
    Co-authored-by: Tommy Song <tommysong123@gmail.com>

--- __tests__/serverTest.js
+++
@@ -1,9 +0,0 @@
-import server from '../server/server';
-import * as request from 'supertest';
-
-describe('server router integration', () => {
-  describe('/refresh', () => {});
-  describe('/connect', () => {});
-  describe('/newSchema', () => {});
-  describe('/results', () => {});
-});
